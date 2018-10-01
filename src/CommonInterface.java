553f586a03cdc70ca4080eb6722c33c0e43ed350
klynx <KevinLynx_L@hotmail.com>
2018-09-30 19:22:50 -0700

    Added function specs

--- src/CommonInterface.java
+++ src/CommonInterface.java
@@ -3,8 +3,38 @@ import java.io.IOException;
 
 
 public interface CommonInterface {
+    /**
+     *
+     * @param x: The input Vector. double array.
+     * @return Value returned by th LUT or NN for input vector
+     */
     public double outputFor(double [] x);
+
+
+    /**
+     * Method tells NN or LUT output value to map to input vector
+     * ex. The desired correct output value for given input
+     * @param x: The input vector
+     * @param argvalue: The new value to learn
+     * @return The error of output for input vector
+     */
     public double train(double [] x, double argvalue);
+
+
+    /**
+     * Write either LUT or weights to for the NN to a file
+     * @param argFile: type file input
+     */
     public void save(File argFile);
+
+    /**
+     * Loads LUT / NN weights from given file. Load have knowledge of how data
+     * was written by save method. Raise error when trying to load incorrect format
+     * ex. Wrong number of neurons
+     * @param argFileName
+     * @throws IOException
+     */
     public void load(String argFileName) throws IOException;
 }
+
+
