987749f363b86653c1430a711f8c9be2a51829d3
klynx <KevinLynx_L@hotmail.com>
2018-10-22 00:56:11 -0700

    Added in training functionality

--- src/XOR/NeuralNet.java
+++ src/XOR/NeuralNet.java
@@ -7,17 +7,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.Random;
 
 public class NeuralNet implements NeuralNetInterface{
-    final double bias = 1.0;
+    public double bias = 1.0;
 
     public double a;
     public double b;
     public double b_minus_a;
     public double minus_a;
-    public double LearningRate;
-    public double MomentumTerm;
+    public double learningRate;
+    public double momentumTerm;
     public int numInputs;
     public int numHidden;
 
@@ -27,7 +26,20 @@ public class NeuralNet implements NeuralNetInterface{
     public double[] weightHiddenToOutput;
     public double[] previousWeightHiddenToOutput;
 
-    private double [] hiddenNeuron; 					// hidden neuron vector
+    public double [] hiddenNeuron; 					// hidden neuron vector
+
+
+    public double[][] inputValues;
+    public double[] targetValues;
+    public double tmpOut = 0;
+    public double outputError = 0;
+    public double[] hiddenError;
+
+    // temp store current weight, used in momentum
+    public double[][] tempWeightInputToHidden;
+    public double[] tempWeightHiddenToOutput;
+    public double[] hiddenValues;
+
 
     /**
      * Constructor for NeuralNet
@@ -46,8 +58,8 @@ public class NeuralNet implements NeuralNetInterface{
         b_minus_a = b - a;
         minus_a = -1 * a;
 
-        LearningRate = argLearningRate;
-        MomentumTerm = argMomentumTerm;
+        learningRate = argLearningRate;
+        momentumTerm = argMomentumTerm;
 
         numInputs = argNumInputs;
         numHidden = argNumHidden;
@@ -58,7 +70,20 @@ public class NeuralNet implements NeuralNetInterface{
         weightHiddenToOutput = new double[numHidden + 1];
         previousWeightHiddenToOutput = new double[numHidden + 1];
 
+
+        // initialize hidden layer
         hiddenNeuron = new double [numHidden];
+        hiddenValues = new double[numHidden];
+        inputValues = new double[numHidden][numInputs + 1];
+        targetValues = new double[numHidden];
+        hiddenError = new double[numHidden];
+
+
+        // temp store current weight, used in momentum
+        tempWeightInputToHidden = new double[numHidden][numInputs + 1];
+        tempWeightHiddenToOutput = new double[numHidden + 1];
+
+
     }
 
 
@@ -134,18 +159,18 @@ public class NeuralNet implements NeuralNetInterface{
         }
 
         // Compute weighted sum at hidden neurons
-        for ( int i = 0; i < numHidden; i++ ) {
+        for (int i = 0; i < numHidden; i++){
             weightedSumHidden[i] = 0;
-            for ( int j=0; j<numInputs; j++ ) {
+            for (int j = 0; j < numInputs; j++){
                 weightedSumHidden[i] += x[j] * weightInputToHidden[i][j];
             }
             weightedSumHidden[i] += bias * weightInputToHidden[i][numInputs];
-            hiddenNeuron[i] = sigmoid( weightedSumHidden[i]);
+            hiddenNeuron[i] = sigmoid(weightedSumHidden[i]);
         }
 
         // Compute weighted sum of output neuron
         weightedSumOutput = 0;
-        for ( int i = 0; i < numHidden; i++ )
+        for (int i = 0; i < numHidden; i++)
             weightedSumOutput += hiddenNeuron[i] * weightHiddenToOutput[i];
         weightedSumOutput += bias * weightHiddenToOutput[numHidden];
 
@@ -163,26 +188,128 @@ public class NeuralNet implements NeuralNetInterface{
      * @return The error of output for input vector
      */
     public double train(double [] x, double argvalue){
-        double actualOutput = outputFor(x);
-        double errorAtOutput = argvalue - actualOutput;
+        // feed forward
+        calcHiddenValue(x, argvalue);
+        calcOutputValue();
 
-        // Derivative for a general sigmoid bounded by the range (a,b). See Fausett pg 309 for details.
-        double sigmoidPrimeAtOutput = (1 / b_minus_a ) * (minus_a + actualOutput) * (b_minus_a - minus_a - actualOutput);
-        double deltaAtOutput = errorAtOutput * sigmoidPrimeAtOutput; // delta is the error signal
+        // backPrapogation
+        calcOutputError(argvalue);
+        calcHiddenError();
 
-        double weightChange = 0;
-
-        if (argvalue > b) {
-            System.out.println("*** NeuralNet: Target of " + argvalue + " exceeds upper bound of " + b);
-        }
-        if (argvalue < a) {
-            System.out.println("*** NeuralNet: Target of " + argvalue + " exceeds lower bound of " + a);
-        }
-
-        return errorAtOutput;
+        // update weights
+        updateWeightInputToHidden(x);
+        updateWeightHiddenToOutput();
+        return 0;
     }
 
+    private void calcHiddenValue(double[] X, double target) {
+        for (int i = 0; i < numHidden; i++) {
+            hiddenValues[i] = 0;
+            for (int j = 0; j < numInputs; j++) {
+                hiddenValues[i] += X[j] * weightInputToHidden[i][j];
+            }
+            hiddenValues[i] += weightInputToHidden[i][numInputs] * 1; // bias
 
+            // processed by activation function
+            hiddenValues[i] = sigmoid(hiddenValues[i]);
+        }
+    }
+
+    private void calcOutputValue() {
+        double tmpOut = 0;
+        for (int i = 0; i < numHidden; i++) {
+            tmpOut += hiddenValues[i] * weightHiddenToOutput[i];
+        }
+        tmpOut += weightHiddenToOutput[numHidden] * 1; // bias
+        tmpOut = sigmoid(tmpOut); // activation function
+    }
+
+    private void calcOutputError(double target) {
+        // for bipolar input
+        outputError = 0.5 * (1 +tmpOut) * (1 - tmpOut) * (target - tmpOut);
+
+        // for binary input
+        //outputError = tmpOut * (1 - tmpOut) * (target - tmpOut);
+        //System.out.println("output error:" + outputError + "\ttmpOut: " + tmpOut + "\ttarget: " + target);
+    }
+
+    private void calcHiddenError() {
+        // for bipolar input
+        for (int i = 0; i < numHidden; i++) {
+            hiddenError[i] = 0.5 * (1 +hiddenValues[i]) * (1 - hiddenValues[i])
+                    * outputError * weightHiddenToOutput[i];
+
+            // for binary input
+//		for (int i = 0; i < numHidden; i++) {
+//			hiddenError[i] = hiddenValues[i] * (1 - hiddenValues[i])
+//					* outputError * weightHiddenToOutput[i];
+            //System.out.println(i + " hidden error" + hiddenError[i]);
+        }
+    }
+
+    private void updateWeightInputToHidden(double[] X) {
+        // // backup current weight
+        for (int i = 0; i < numHidden; i++) {
+            System.arraycopy(weightInputToHidden[i], 0, tempWeightInputToHidden[i], 0,
+                    weightInputToHidden[i].length);
+        }
+
+        // update weightInputToHidden
+        for (int i = 0; i < numHidden; i++) {
+            for (int j = 0; j < numInputs; j++) {
+                weightInputToHidden[i][j] += learningRate
+                        * hiddenError[i] * X[j] + momentumTerm
+                        * getPreviousToHiddenDeltaWeight(i, j);
+                //System.out.println("numHidden: " + i + "  Inputs: " + X[j] + "  weight: " +weightInputToHidden[i][j]);
+            }
+            // for bias term below
+            weightInputToHidden[i][numInputs] += learningRate
+                    * hiddenError[i] + momentumTerm
+                    * getPreviousToHiddenDeltaWeight(i, numInputs);
+            //System.out.println("numHidden: " + i + "  Inputs: " +  X[numInputs] + "  weight: " +weightInputToHidden[i][numInputs]);
+        }
+
+        // update previous weight
+        for (int i = 0; i < numHidden; i++) {
+            System.arraycopy(tempWeightInputToHidden[i], 0, previousWeightInputToHidden[i], 0,
+                    weightInputToHidden[i].length);
+        }
+
+    }
+
+    private void updateWeightHiddenToOutput() {
+        // backup current weight
+        System.arraycopy(weightHiddenToOutput, 0, tempWeightHiddenToOutput, 0,
+                weightHiddenToOutput.length);
+
+        for (int i = 0; i < numHidden; i++) {
+            weightHiddenToOutput[i] += learningRate * outputError
+                    * hiddenValues[i] + momentumTerm
+                    * getPreviousToOutDeltaWeight(i);
+            //System.out.println("numHidden: " + i + "  weight: " +weightHiddenToOutput[i]);
+        }
+        weightHiddenToOutput[numHidden] += learningRate * outputError * 1
+                + momentumTerm * getPreviousToOutDeltaWeight(numHidden);
+        // update previous weight
+        System.arraycopy(tempWeightHiddenToOutput, 0,
+                previousWeightHiddenToOutput, 0,
+                tempWeightHiddenToOutput.length);
+    }
+
+    private double getPreviousToOutDeltaWeight(int i) {
+        if (previousWeightHiddenToOutput[i] != 0)
+            return weightHiddenToOutput[i] - previousWeightHiddenToOutput[i];
+        else
+            return 0;
+    }
+
+    private double getPreviousToHiddenDeltaWeight(int i, int j) {
+        if (previousWeightInputToHidden[i][j] != 0)
+            return weightInputToHidden[i][j]
+                    - previousWeightInputToHidden[i][j];
+        else
+            return 0;
+    }
     /**
      * Write either LUT or weights to for the NN to a file
      * @param argFile: type file input
--- src/XOR/XOR_main.java
+++ src/XOR/XOR_main.java
@@ -6,7 +6,27 @@ public abstract class XOR_main {
     public static void main(String[] args){
         System.out.println("Running NeuralNet");
 
-        // NeuralNet xor = new NeuralNet(2, 4, 0.2, 0.0, -0.5, 0.5);
+        //Run these settings
+        int argNumInputs = 2;
+        int argNumHidden = 4;
+        double argLearningRate = 0.2;
+        double argMomentumTerm = 0.0;
+        double argA = 0;
+        double argB = 1;
+
+        NeuralNet xor = new NeuralNet(argNumInputs, argNumHidden, argLearningRate, argMomentumTerm, argA, argB);
+
+        double[] data1 = {0, 0};
+        double[] data2 = {0, 1};
+        double[] data3 = {1, 0};
+        double[] data4 = {1, 1};
+
+        while(true){
+            System.out.println(xor.train(data1, 0 ));
+            System.out.println(xor.train(data2, 1 ));
+            System.out.println(xor.train(data3, 1 ));
+            System.out.println(xor.train(data4, 0 ));
+        }
     }
     /**
      * Constructor. (Cannot be declared in an interface, but your implementation will need one)
